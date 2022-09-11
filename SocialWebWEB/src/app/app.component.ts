5f3aa1259641c2479329a8b6c036d84a8292b638
zarar384 <fel98_98@mail.ru>
2022-09-11 19:57:23 +0200

    testAppComponent

--- SocialWebWEB/src/app/app.component.ts
+++ SocialWebWEB/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SocialWebWEB';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers()
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5000/api/users')
+      .subscribe({
+        next: res => this.users = res,
+        error: error => console.log(error)
+      })
+  }
 }
