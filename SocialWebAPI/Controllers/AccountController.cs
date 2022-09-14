347f43009442a2c5950be57da6ff7b1ee964dc3f
zarar384 <fel98_98@mail.ru>
2022-09-14 22:51:37 +0200

    register and login function

--- SocialWebAPI/Controllers/AccountController.cs
+++ SocialWebAPI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SocialWebAPI.Db;
+using SocialWebAPI.DTOs;
 using SocialWebAPI.Entities;
 using System.Security.Cryptography;
 using System.Text;
@@ -16,14 +18,16 @@ namespace SocialWebAPI.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            using var hmac = new HMACSHA512();
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
+            using var hmac = new HMACSHA512();
+ 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -31,6 +35,28 @@ namespace SocialWebAPI.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(RegisterDto loginDto)
+        {
+            var user =  _context.AppUsers.Where(x => x.UserName == loginDto.Username).FirstOrDefault();
+
+            if (user == null) return Unauthorized("Invalid username");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+            return Ok(user);
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.AppUsers.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
---
+++ SocialWebAPI/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialWebAPI.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Username { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Password { get; set; }
+
+    }
+}
