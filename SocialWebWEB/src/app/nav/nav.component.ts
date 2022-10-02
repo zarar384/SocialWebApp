4a943d55a0beeb703bd690f8f88705d893f2924b
zarar384 <fel98_98@mail.ru>
2022-10-02 21:44:02 +0200

    correct route to main page

--- SocialWebWEB/src/app/nav/nav.component.ts
+++ SocialWebWEB/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AccountService} from "../_services/account.service";
 import {observable, Observable} from "rxjs";
 import { User } from '../_models/user';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
   model: any ={}
   userName: any;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
     this.setUsername()
@@ -21,6 +22,7 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response =>{
       this.setUsername()
+      this.router.navigateByUrl('/members')
       console.log(response)
     }, error =>{
       console.log(error)
@@ -29,6 +31,7 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout()
+    this.router.navigateByUrl('/')
   }
 
   setUsername(){
