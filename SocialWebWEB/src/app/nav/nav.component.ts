fcc33e19141ec070b84a5c13b1389709945e8d83
zarar384 <fel98_98@mail.ru>
2022-10-02 22:11:04 +0200

    add notification

--- SocialWebWEB/src/app/nav/nav.component.ts
+++ SocialWebWEB/src/app/nav/nav.component.ts
@@ -3,7 +3,7 @@ import {AccountService} from "../_services/account.service";
 import {observable, Observable} from "rxjs";
 import { User } from '../_models/user';
 import {Router} from "@angular/router";
-
+import {ToastrService} from 'ngx-toastr'
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -13,7 +13,8 @@ export class NavComponent implements OnInit {
   model: any ={}
   userName: any;
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.setUsername()
@@ -26,6 +27,7 @@ export class NavComponent implements OnInit {
       console.log(response)
     }, error =>{
       console.log(error)
+      this.toastr.error(error.error)
     })
   }
 
--- SocialWebWEB/src/app/register/register.component.ts
+++ SocialWebWEB/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountService} from "../_services/account.service";
-
+import {ToastrService} from 'ngx-toastr'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,7 +10,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter()
   model:any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +21,7 @@ export class RegisterComponent implements OnInit {
       this.cancel()
     }, error=>{
       console.log(error)
+      this.toastr.error(error.error)
     })
   }
 
