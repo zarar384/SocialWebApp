c4679cea87973298f727f36e198adce6cc46a501
zarar384 <fel98_98@mail.ru>
2022-10-05 22:01:04 +0200

    add new controller - BuggyController

---
+++ SocialWebAPI/Controllers/BuggyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialWebAPI.Db;
+
+namespace SocialWebAPI.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+        private readonly AppDbContext _context;
+
+        public BuggyController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+    }
+}
