8b505c3331e9756b14c3f909a89b3de582ca0863
zarar384 <fel98_98@mail.ru>
2023-01-20 22:00:10 +0100

    Web Add user/member models and add meber service

---
+++ SocialWebWEB/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import {Photo} from "src/app/_models/photo"
+
+export interface Member {
+  appUserId: number;
+  userName: string;
+  photoUrl: string;
+  age: number;
+  knownAs: string;
+  created: Date;
+  lastActive: Date;
+  gender: string;
+  introduction: string;
+  lookingFor: string;
+  interests: string;
+  city: string;
+  country: string;
+  photos: Photo[];
+}
