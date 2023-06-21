cd7c6ab6688a9e059e026b272597019837972a34
zarar384 <fel98_98@mail.ru>
2023-06-21 23:26:01 +0200

    addind deactivate route guard

--- SocialWebWEB/src/app/members/member-edit/member-edit.component.ts
+++ SocialWebWEB/src/app/members/member-edit/member-edit.component.ts
@@ -1,43 +1,53 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Member} from 'src/app/_models/member';
-import {User} from 'src/app/_models/user';
-import {AccountService} from "../../_services/account.service";
-import {MembersService} from "../../_services/members.service";
-import {take} from "rxjs";
-import {ToastrModule, ToastrService} from "ngx-toastr";
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { take } from 'rxjs';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
-  styleUrls: ['./member-edit.component.css']
+  styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editFrom') editForm: NgForm
+  @ViewChild('editFrom') editForm: NgForm;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | null = null;
 
-  constructor(private accountService: AccountService, private memberService: MembersService,
-              private toastr: ToastrService) {
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => this.user = user
-    })
+      next: (user) => (this.user = user),
+    });
   }
 
   ngOnInit(): void {
-    this.loadMember()
+    this.loadMember();
   }
 
-  loadMember(){
-    if(!this.user) return;
+  loadMember() {
+    if (!this.user) return;
     this.memberService.getMember(this.user.username).subscribe({
-      next: member => this.member = member
-    })
+      next: (member) => (this.member = member),
+    });
   }
 
-  updateMember(){
+  updateMember() {
     console.log(this.member);
-    this.toastr.success('Profile updated successfully')
-    this.editForm?.reset(this.member)
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
