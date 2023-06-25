ee9764bbaaa2736713a5e5e75a6f79fe207b969c
zarar384 <fel98_98@mail.ru>
2023-06-25 23:24:46 +0200

    updateMember

--- SocialWebWEB/src/app/_services/members.service.ts
+++ SocialWebWEB/src/app/_services/members.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environments/environment";
-import {HttpClient, HttpHandler, HttpHeaders} from "@angular/common/http";
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Member } from '../_models/member';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class MembersService {
-  baseUrl = environment.apiUrl
+  baseUrl = environment.apiUrl;
 
-  constructor( private  http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+ 'users')
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
-  getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'users/' + username)
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member);
   }
 
   // getHttpOptions(){
