4166dfd20194c914f9f4ccdc814051acdde4f18a
zarar384 <fel98_98@mail.ru>
2023-07-01 21:56:55 +0200

    add PhotoService

--- SocialWebAPI/Extensions/ApplicationServiceExtensions.cs
+++ SocialWebAPI/Extensions/ApplicationServiceExtensions.cs
@@ -10,10 +10,12 @@ namespace SocialWebAPI.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<AppDbContext>(option =>
-            option.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+                option.UseSqlServer(config.GetConnectionString("DefaultConnection")));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
             return services;
         }
---
+++ SocialWebAPI/Services/PhotoService.cs
@@ -0,0 +1,46 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+
+namespace SocialWebAPI;
+
+public class PhotoService : IPhotoService
+{
+    private readonly Cloudinary _cloudinary;
+    public PhotoService(IOptions<CloudinarySettings> config)
+    {
+        var acc = new Account(
+            config.Value.CloudName,
+            config.Value.ApiKey,
+            config.Value.ApiSecret);
+
+        _cloudinary = new Cloudinary(acc);
+    }
+
+    public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+    {
+        var uplResult = new ImageUploadResult();
+
+        if (file.Length > 0)
+        {
+            using var steam = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, steam),
+                Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
+                Folder = "da-net7"
+            };
+
+            uplResult = await _cloudinary.UploadAsync(uploadParams);
+        }
+
+        return uplResult;
+    }
+
+    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+    {
+        var delParams = new DeletionParams(publicId);
+
+        return await _cloudinary.DestroyAsync(delParams);
+    }
+}
