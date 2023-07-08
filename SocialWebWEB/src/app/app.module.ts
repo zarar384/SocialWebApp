218342a87420df1a5d874a3151c2f1d64a9fe955
zarar384 <fel98_98@mail.ru>
2023-07-08 23:07:50 +0200

    register form

--- SocialWebWEB/src/app/app.module.ts
+++ SocialWebWEB/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -49,6 +46,7 @@ import PhotoEditorComponent from './members/photo-editor/photo-editor.component'
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
   ],
   providers: [
--- SocialWebWEB/src/app/register/register.component.ts
+++ SocialWebWEB/src/app/register/register.component.ts
@@ -1,31 +1,48 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {AccountService} from "../_services/account.service";
-import {ToastrService} from 'ngx-toastr'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  @Output() cancelRegister = new EventEmitter()
-  model:any = {}
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
-  register(){
-    this.accountService.register(this.model).subscribe(response=>{
-      console.log(response);
-      this.cancel()
-    }, error=>{
-      console.log(error)
-      this.toastr.error(error.error)
-    })
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
   }
 
-  cancel(){
-    this.cancelRegister.emit(false)
+  register() {
+    // this.accountService.register(this.model).subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //     this.toastr.error(error.error);
+    //   }
+    // );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
   }
 }
