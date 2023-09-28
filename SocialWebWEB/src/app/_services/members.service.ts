61d23b18af049492a8f5beac6716ff7c02d15a33
zarar384 <fel98_98@mail.ru>
2023-09-28 19:43:45 +0200

    update list component

--- SocialWebWEB/src/app/_services/members.service.ts
+++ SocialWebWEB/src/app/_services/members.service.ts
@@ -131,13 +131,17 @@ export class MembersService {
   }
 
   addLike(username: string) {
-    return this.http.post(this.baseUrl + 'like/' + username, {});
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
   }
 
-  getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + 'likes?predicate=' + predicate);
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
-  
+
   // getHttpOptions(){
   //   const userString = localStorage.getItem('user');
   //   if(!userString) return;
--- SocialWebWEB/src/app/members/members-card/members-card.component.ts
+++ SocialWebWEB/src/app/members/members-card/members-card.component.ts
@@ -20,7 +20,7 @@ export class MembersCardComponent implements OnInit {
 
   addLike(member: Member) {
     this.memberService.addLike(member.userName).subscribe({
-      next: () => this.toastr.success('You have liked' + member.knownAs),
+      next: () => this.toastr.success('You have liked ' + member.knownAs),
     });
   }
 }
