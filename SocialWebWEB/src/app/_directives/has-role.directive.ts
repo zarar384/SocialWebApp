29d459e507f6cde6d799e3d35dc6a2646a7a40c5
zarar384 <fel98_98@mail.ru>
2023-10-22 17:41:27 +0200

    add custom directive

--- SocialWebWEB/src/app/_directives/has-role.directive.ts
+++ SocialWebWEB/src/app/_directives/has-role.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+import { take } from 'rxjs';
 
+//*appHasRole = '["Admin", "Thing"]'
 @Directive({
-  selector: '[appHasRole]'
+  selector: '[appHasRole]',
 })
-export class HasRoleDirective {
+export class HasRoleDirective implements OnInit {
+  @Input() appHasRole: string[] = [];
+  user: User = {} as User;
 
-  constructor() { }
-
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+    private accountService: AccountService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) this.user = user;
+      },
+    });
+  }
+  ngOnInit(): void {
+    if (this.user.roles.some((r) => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
 }
