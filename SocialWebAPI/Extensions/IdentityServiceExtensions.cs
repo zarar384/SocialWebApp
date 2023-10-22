abbb6d0fa0e673815b2254a9ec16130e48136419
zarar384 <fel98_98@mail.ru>
2023-10-22 17:10:38 +0200

    edit user roles

--- SocialWebAPI/Extensions/IdentityServiceExtensions.cs
+++ SocialWebAPI/Extensions/IdentityServiceExtensions.cs
@@ -30,6 +30,13 @@ namespace SocialWebAPI.Extensions
                         ValidateAudience = false,
                     };
                 });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
