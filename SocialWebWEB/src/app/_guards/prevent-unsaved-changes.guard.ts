1348053e890f25176bf89f63e6ef8e85370bdeb6
zarar384 <fel98_98@mail.ru>
2023-11-05 17:06:44 +0100

    added confirm component

--- SocialWebWEB/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ SocialWebWEB/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
+import { inject } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class PreventUnsavedChangesGuard
-  implements CanDeactivate<MemberEditComponent>
-{
-  canDeactivate(component: MemberEditComponent): boolean {
-    if (component.editForm?.dirty) {
-      return confirm('Are you sure? Unsaved changes will be lost');
-    }
-    return true;
+export const preventUnsavedChangesGuard: CanDeactivateFn<
+  MemberEditComponent
+> = (component) => {
+  const confirmService = inject(ConfirmService);
+
+  if (component.editForm?.dirty) {
+    return confirmService.confirm();
   }
-}
+
+  return true;
+};
--- SocialWebWEB/src/app/app-routing.module.ts
+++ SocialWebWEB/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { adminGuard } from './_guards/admin.guard';
@@ -35,7 +35,7 @@ const routes: Routes = [
       {
         path: 'member/edit',
         component: MemberEditComponent,
-        canDeactivate: [PreventUnsavedChangesGuard],
+        canDeactivate: [preventUnsavedChangesGuard],
       },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
